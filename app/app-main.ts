2dfe0435fe0abde5f354d1b85f4b3359895a1a80
Martin Kvasnička <mkvasnicka@mlc.cz>
2017-08-04 08:52:50 +0200

    Observable example

--- app/app-main.ts
+++ app/app-main.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+import { Observer } from 'rxjs/Observer'
 
 const templateUrl = './app-main.htm';
 @Component({
@@ -6,4 +8,26 @@ const templateUrl = './app-main.htm';
 	moduleId: module.id,
 	templateUrl: templateUrl
 })
-export class AppMainComponent {}
+export class AppMainComponent {
+	name: string;
+	bar: Observable<number>;
+	constructor() {
+		this.name = `Angular! v${VERSION.full}`
+	}
+
+	MyClick() {
+		this.bar = Observable.create((observer: Observer<number>) => {
+			console.log('Hello');
+			observer.next(42);
+			observer.next(100);
+			observer.next(200);
+			setTimeout(() => {
+				observer.next(300);
+			}, 1000);
+		});
+
+		console.log('before');
+		this.bar.subscribe((x) => console.log(x));
+		console.log('after');
+	}
+}
